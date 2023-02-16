3e7fb44a540fea9ee19ccb1520ebe0f7882b71c2
Kartu2 <kartuissah@gmail.com>
2023-02-16 11:51:18 -0800

    5-print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
  * main - print "with proper grammar, but the 
  * outcome is a piece of art," followed by a newline
  * Return: Always 0.
- *
  */
 
 int main(void)
@@ -12,5 +11,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}
-		
+}	
