b80bc1967c027b4e9eb7aebb4321f4bf2a64d12c
Kartu2 <kartuissah@gmail.com>
2023-02-18 07:04:03 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *  main - Entry point 
+ *  main - Entry point
  *
  *  Return: Always 0 (success)
  */
@@ -17,7 +17,7 @@ int main(void)
 			putchar(c);
 			c++;
 		}
-	while 
+	while
 		(d <= 'Z') {
 			putchar(d);
 			d++;
