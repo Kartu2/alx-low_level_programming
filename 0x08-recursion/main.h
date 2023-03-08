ac8f55b5247056f52ef02c06d23ab24af7e750c1
Kartu2 <kartuissah@gmail.com>
2023-03-08 11:42:03 -0800

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,6 +6,7 @@
  * Auth: me
  * Desc: Header file containing prototypes of all functions
  */
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
