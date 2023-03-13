c5457cdc2a078c0a372a22aa48aa7a0cd446b606
Kartu2 <kartuissah@gmail.com>
2023-03-13 12:54:03 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -32,5 +32,30 @@ int _atoi(char *s)
 			f = 1;
 		}
 	}
+	return (n);
+}
+
+/**
+ * main - multiples two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Retur: 0 (success), 1 (error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
 	return (0);
 }
